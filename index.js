63fc5f16e6b5760d86336f4031b09cb7837a55f6
JdavisBro <44114870+JdavisBro@users.noreply.github.com>
2021-07-02 20:23:42 +0100

    Accidentally left a debug thing enabled

--- index.js
+++ index.js
@@ -216,7 +216,7 @@ document.getElementById('map').onclick = function clickEvent(e) {
     screenPos[1] = 1080 * (screen[2] - Math.floor(screen[2]))
     screen[2] = Math.floor(screen[2])
     document.getElementById("clickedPos").innerHTML = "Last Clicked - Screen: L " + screen[0] + " X " + screen[1] + " Y " + screen[2] + " Pos: X " + Math.round(screenPos[0]*100)/100 + " Y " + Math.round(screenPos[1]*100)/100;
-    navigator.clipboard.writeText(',\n{\n    "id": "'+screen[0]+"_"+screen[1]+"_"+screen[2]+'_0",\n    "direction": "up",\n    "screen": "'+screen[0]+"_"+screen[1]+"_"+screen[2]+'",\n    "x": '+screenPos[0]+',\n    "y": '+screenPos[1]+',\n    "to": ""\n}'); // This is for when i add the entrances to the map, this will take a long time AHHHH
+    //navigator.clipboard.writeText(',\n{\n    "id": "'+screen[0]+"_"+screen[1]+"_"+screen[2]+'_0",\n    "direction": "up",\n    "screen": "'+screen[0]+"_"+screen[1]+"_"+screen[2]+'",\n    "x": '+screenPos[0]+',\n    "y": '+screenPos[1]+',\n    "to": ""\n}'); // This is for when i add the entrances to the map, this will take a long time AHHHH
 }
 
 window.addEventListener("load",makeMap);
